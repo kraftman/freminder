20d61ab435f47832e27c65ce8a80d3376c5de233
Chris Tanner <me@itschr.is>
2017-03-13 19:46:52 +0000

    added startup script, split docker-compose envs to new file

---
+++ startdev.sh
@@ -0,0 +1,3 @@
+docker-compose -f docker-compose.yml -f docker-compose-env.yml stop
+docker-compose -f docker-compose.yml -f docker-compose-env.yml rm -f
+docker-compose -f docker-compose.yml -f docker-compose-env.yml up
